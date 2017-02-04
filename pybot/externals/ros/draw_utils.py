b0bc0be7675642147a9cce23f86220872f6d9da0
Oscar Moll <orm@csai.mit.edu>
2017-02-04 21:33:00 +0000

    dont init implicitly

--- pybot/externals/ros/draw_utils.py
+++ pybot/externals/ros/draw_utils.py
@@ -31,7 +31,8 @@ def init():
     try: 
         # Initialize node
         import rospy
-        rospy.init_node('pybot_vision_draw_utils_node', anonymous=True, disable_signals=True)
+        # ORM: double init node is causing problems.
+        #rospy.init_node('pybot_vision_draw_utils_node', anonymous=True, disable_signals=True)
 
         global viz_pub_
         viz_pub_ = VisualizationMsgsPub()
